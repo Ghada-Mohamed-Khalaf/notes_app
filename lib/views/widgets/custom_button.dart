e026620501c03ce694a624ee9ac105e2ab550638
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-04-19 22:44:22 +0200

    finsh note bottom sheet

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/constant.dart';
+
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      width:MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: KPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: const Center(child: Text("add",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),)),
+
+
+    );
+  }
+}

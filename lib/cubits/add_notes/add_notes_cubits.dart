abad1c81c58edfdb5886e1ead68709cb66f79dda
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-04-28 23:57:18 +0300

    add addNote_cubit method addNote cubit

--- lib/cubits/add_notes/add_notes_cubits.dart
+++ lib/cubits/add_notes/add_notes_cubits.dart
@@ -1,13 +1,36 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
 import 'package:notes_app/cubits/add_notes/add_note_state.dart';
 import 'package:notes_app/cubits/add_notes/add_note_state.dart';
 import 'package:notes_app/cubits/add_notes/add_note_state.dart';
 import 'package:notes_app/cubits/add_notes/add_note_state.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/constant.dart';
+
+import 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState>{
   AddNoteCubit():super(AddNoteInitial());
-  addNote(NoteModel note)
+  addNote(NoteModel note)async{
+    emit(AddNoteLoading());
+
+
+    try {
+      var notesBox = Hive.box(kNotesBox);
+      emit(AddNoteSuccess())
+         await notesBox.add(note);
+    } on Exception catch (e) {
+      AddNoteFailure(e.toString());
+      // TODO
+    }
+
+  }
 
 }
 

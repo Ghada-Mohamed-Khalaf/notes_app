514355adb300dc8e47946c6a9d7198a3a266130d
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-05-02 23:10:48 +0300

    add blocConsumer

--- lib/views/widgets/note_bottom_sheet.dart
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -1,84 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_notes/add_note_state.dart';
+import 'package:notes_app/cubits/add_notes/add_notes_cubits.dart';
+import 'package:notes_app/views/widgets/add_note_form.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
-class AddNoteBottomSheet extends StatefulWidget {
+class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
-  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
-}
-
-class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
-  @override
   Widget build(BuildContext context) {
-    return
-      AddNoteForm();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child:  BlocConsumer<AddNoteCubit,AddNoteState>(
+        listener: (context,state){
+
+        },
+        builder: (context,state){
 
 
+   return ModalProgressHUD( inAsyncCall: ,
+       child: const AddNoteForm());
+    },
+      ),
 
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
- final GlobalKey<FormState> formKey = GlobalKey();
- AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
- String? title,subTitle;
-
- @override
-  Widget build(BuildContext context) {
-    return  Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            autovalidateMode:autovalidateMode ,
-            child: Column(
-              children: [
-                SizedBox(height: 30.h,),
-                CustomTextField(
-                  onSaved: (value){
-                    title=value;
-                  },
-
-                  hint: 'title',),
-                SizedBox(height: 16.h,),
-                CustomTextField(
-                  onSaved: (value){
-                    subTitle=value;
-                  },
-
-                  hint: 'content',maxLines: 5,),
-                SizedBox(height: 30.h,),
-            
-                CustomButton(
-                  onTap:(){
-                    if(formKey.currentState!.validate()){
-                      formKey.currentState!.save();
-                    }else{
-                      autovalidateMode=AutovalidateMode.always;
-                      setState(() {
-
-                      });
-                    }
-                  },
-                ),
-
-              ],
-            ),
-          ),
-        ),
     );
+
+
+
   }
 }
+

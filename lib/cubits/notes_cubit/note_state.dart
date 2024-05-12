9d905287fbb1a75d1aeae4f61939804ae2b01131
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-05-12 23:22:41 +0300

    delete noteSuccess

--- lib/cubits/notes_cubit/note_state.dart
+++ lib/cubits/notes_cubit/note_state.dart
@@ -3,9 +3,7 @@
 import 'package:notes_app/models/note_model.dart';
 
 abstract class NoteState{}
-class NoteInitial extends NoteState{}
-class NoteLoading extends NoteState{}
-class NoteSuccess extends NoteState{
+
   final  List<NoteModel> notes;
 
   NoteSuccess(this.notes);
